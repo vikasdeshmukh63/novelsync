ca8501cc4fa6d194d583aded7acb186a14a78e9e
amithsulakhe <amithsulakhe729@gmail.com>
2024-11-11 18:16:37 +0530

    minor chanege

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -5,5 +5,5 @@ import { BlankView } from 'src/sections/blank/view';
 export const metadata = { title: `Dashboard - NovelSync` };
 
 export default function Page() {
-  return <BlankView title="Page one" />;
+  return <BlankView title="Dashboard" />;
 }

a90224fe4f7e4f143e5a26d6740cd81b5cfeabff
amithsulakhe <amithsulakhe729@gmail.com>
2024-11-11 18:15:20 +0530

    removed default values

--- src/app/auth/signup/page.jsx
+++ src/app/auth/signup/page.jsx
@@ -1,10 +1,8 @@
-import { CONFIG } from 'src/config-global';
-
 import { JwtSignUpView } from 'src/auth/view/jwt';
 
 // ----------------------------------------------------------------------
 
-export const metadata = { title: `Sign up | Jwt - ${CONFIG.appName}` };
+export const metadata = { title: `Sign up : NovelSync` };
 
 export default function Page() {
   return <JwtSignUpView />;
